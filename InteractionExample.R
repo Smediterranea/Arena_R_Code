ad8af74e321467e673a016b93b14d3832f3c77fd
Scott Pletcher <spletch@umich.edu>
2022-01-18 10:39:21 -0500

    Updated

--- InteractionExample.R
+++ InteractionExample.R
@@ -1,25 +1,40 @@
-source("ParametersClass.R")
-source("InteractionCounter.R")
 require(ggplot2)
 
+source("ParametersClass.R")
+source("InteractionCounter.R")
 
-datadir<-"Data"
+
+##***********
+datadir<-"Run1"
 distance.for.interaction.mm <-8
+binsize.in.min<-5
+##***********
 
+## Make sure to set the mmPerPixel parameter to be correct.
+p<-Parameters.SetParameter(mmPerPixel = 0.056)
+
+## Run these next four functions to get the data saved to files in data directory.
+
+##***********
 p<-ParametersClass.InteractionCounter()
 interaction.results<-InteractionCounterData(p,datadir,distance.for.interaction.mm)
 
-
-binned.interaction.results<-GetBinnedInteractionTime(interaction.results,binsize.min=5)
+binned.interaction.results<-GetBinnedInteractionTime(interaction.results,binsize.min=binsize.in.min)
 write.csv(binned.interaction.results,file=paste(datadir,"/BinnedResults.csv",sep=""),row.names=FALSE)
+##***********
 
-## Other functions of use
-newcutoff.mm<-10
+##***********
+## Run the following three lines if all you want to change is the interaction distance.
+newcutoff.mm<-20
 interaction.results<-UpdateDistanceCutoff(interaction.results,newcutoff.mm)
 binned.interaction.results<-GetBinnedInteractionTime(interaction.results,binsize.min=5)
+##***********
 
+##***********
 ## Plots
-ggplot(results$Results, aes(x=as.numeric(ElapsedTimeMin),y=as.numeric(IsInteracting))) + geom_step()
+ggplot(interaction.results$Results, aes(x=as.numeric(ElapsedTimeMin),y=as.numeric(IsInteracting))) + geom_step()
+
+ggplot(interaction.results$Results, aes(x=as.numeric(ElapsedTimeMin),y=as.numeric(Distance))) + geom_step()
 
 ggplot(binned.interaction.results, aes(x=MidPoint,y=PercentageInteraction)) + geom_step()
-
+##***********
