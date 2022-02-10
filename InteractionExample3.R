5796ed3dbae11277e76c956686f89aea35369994
Scott Pletcher <spletch@umich.edu>
2022-02-10 10:26:41 -0500

    Updated Movie Example

--- InteractionExample3.R
+++ InteractionExample3.R
@@ -9,23 +9,73 @@ datadir<-"Data"
 distance.for.interaction.mm <-8
 binsize.in.min<-5
 ##***********
-
-## Run these next four functions to get the data saved to files in data directory.
+##***********
+##* For movies with old cameras
 
 ##***********
 p<-ParametersClass.InteractionCounter()
+p<-Parameters.SetParameter(p,mmPerPixel=0.2156)
 p<-Parameters.SetParameter(p,FPS=10)
 ##***********
 
+## Run these next four functions to get the data saved to files in data directory.
+interaction.results.c0<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_0")
+binned.interaction.results.c0 <-
+  GetBinnedInteractionTime(interaction.results.c0, binsize.min = binsize.in.min)
+write.csv(
+  binned.interaction.results.left,
+  file = paste(datadir, "/BinnedResultsC0.csv", sep = ""),
+  row.names = FALSE
+)
+
 interaction.results.c1<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_1")
+binned.interaction.results.c1 <-
+  GetBinnedInteractionTime(interaction.results.c1, binsize.min = binsize.in.min)
+write.csv(
+  binned.interaction.results.left,
+  file = paste(datadir, "/BinnedResultsC1.csv", sep = ""),
+  row.names = FALSE
+)
+
 interaction.results.c2<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_2")
+binned.interaction.results.c2 <-
+  GetBinnedInteractionTime(interaction.results.c2, binsize.min = binsize.in.min)
+write.csv(
+  binned.interaction.results.left,
+  file = paste(datadir, "/BinnedResultsC2.csv", sep = ""),
+  row.names = FALSE
+)
 interaction.results.c3<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_3")
+binned.interaction.results.c3 <-
+  GetBinnedInteractionTime(interaction.results.c3, binsize.min = binsize.in.min)
+write.csv(
+  binned.interaction.results.left,
+  file = paste(datadir, "/BinnedResultsC3.csv", sep = ""),
+  row.names = FALSE
+)
 interaction.results.c4<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_4")
+binned.interaction.results.c4 <-
+  GetBinnedInteractionTime(interaction.results.c4, binsize.min = binsize.in.min)
+write.csv(
+  binned.interaction.results.left,
+  file = paste(datadir, "/BinnedResultsC4.csv", sep = ""),
+  row.names = FALSE
+)
 interaction.results.c5<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_5")
+binned.interaction.results.c5 <-
+  GetBinnedInteractionTime(interaction.results.c5, binsize.min = binsize.in.min)
+write.csv(
+  binned.interaction.results.left,
+  file = paste(datadir, "/BinnedResultsC5.csv", sep = ""),
+  row.names = FALSE
+)
 interaction.results.c6<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_6")
-interaction.results.c7<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_7")
-interaction.results.c8<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_8")
-interaction.results.c9<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_9")
-interaction.results.c10<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_10")
-interaction.results.c11<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_11")
-interaction.results.c12<-InteractionCounterData(p,datadir,distance.for.interaction.mm,counting.region = "C_12")
+binned.interaction.results.c6 <-
+  GetBinnedInteractionTime(interaction.results.c6, binsize.min = binsize.in.min)
+write.csv(
+  binned.interaction.results.left,
+  file = paste(datadir, "/BinnedResultsC6.csv", sep = ""),
+  row.names = FALSE
+)
+
+
