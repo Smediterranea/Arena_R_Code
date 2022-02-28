b3097cd0c4838629c0dbe50f6f7e0abe1e741c45
Scott Pletcher <spletch@umich.edu>
2022-02-28 14:47:13 -0500

    Update

--- InteractionExample3.R
+++ InteractionExample3.R
@@ -19,7 +19,7 @@ p<-Parameters.SetParameter(p,FPS=10)
 ##***********
 
 ## Run these next four functions to get the data saved to files in data directory.
-interaction.results.c0<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "C_0")
+interaction.results.c0<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "T_0")
 binned.interaction.results.c0 <-
   GetBinnedInteractionTime(interaction.results.c0, binsize.min = binsize.in.min)
 write.csv(
@@ -28,7 +28,7 @@ write.csv(
   row.names = FALSE
 )
 
-interaction.results.c1<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "C_1")
+interaction.results.c1<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "T_1")
 binned.interaction.results.c1 <-
   GetBinnedInteractionTime(interaction.results.c1, binsize.min = binsize.in.min)
 write.csv(
@@ -37,7 +37,7 @@ write.csv(
   row.names = FALSE
 )
 
-interaction.results.c2<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "C_2")
+interaction.results.c2<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "T_2")
 binned.interaction.results.c2 <-
   GetBinnedInteractionTime(interaction.results.c2, binsize.min = binsize.in.min)
 write.csv(
@@ -45,7 +45,7 @@ write.csv(
   file = paste(datadir, "/BinnedResultsC2.csv", sep = ""),
   row.names = FALSE
 )
-interaction.results.c3<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "C_3")
+interaction.results.c3<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "T_3")
 binned.interaction.results.c3 <-
   GetBinnedInteractionTime(interaction.results.c3, binsize.min = binsize.in.min)
 write.csv(
@@ -53,7 +53,7 @@ write.csv(
   file = paste(datadir, "/BinnedResultsC3.csv", sep = ""),
   row.names = FALSE
 )
-interaction.results.c4<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "C_4")
+interaction.results.c4<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "T_4")
 binned.interaction.results.c4 <-
   GetBinnedInteractionTime(interaction.results.c4, binsize.min = binsize.in.min)
 write.csv(
@@ -61,7 +61,7 @@ write.csv(
   file = paste(datadir, "/BinnedResultsC4.csv", sep = ""),
   row.names = FALSE
 )
-interaction.results.c5<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "C_5")
+interaction.results.c5<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "T_5")
 binned.interaction.results.c5 <-
   GetBinnedInteractionTime(interaction.results.c5, binsize.min = binsize.in.min)
 write.csv(
@@ -69,7 +69,7 @@ write.csv(
   file = paste(datadir, "/BinnedResultsC5.csv", sep = ""),
   row.names = FALSE
 )
-interaction.results.c6<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "C_6")
+interaction.results.c6<-InteractionCounterData(p,datadir,distance.for.interaction.mm,tracking.region = "T_6")
 binned.interaction.results.c6 <-
   GetBinnedInteractionTime(interaction.results.c6, binsize.min = binsize.in.min)
 write.csv(
