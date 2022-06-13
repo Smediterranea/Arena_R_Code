f3069ab90ea5820a7c61f9c5e7e36b66c5093edb
Scott Pletcher <spletch@umich.edu>
2022-06-13 12:23:20 -0400

    Update Example_PairwiseInteraction.R
    Updates

--- Example_PairwiseInteraction.R
+++ Example_PairwiseInteraction.R
@@ -1,3 +1,7 @@
+
+###################################
+## Run this part first
+## If no errors, then move on....
 rm(list=ls())
 ## Do one of the following two
 source("ArenaObject.R")
@@ -21,22 +25,31 @@ p<-Parameters.SetParameter(p,FPS=NA)
 # mm.per.pixel<-0.0.056
 p<-Parameters.SetParameter(p,mmPerPixel=0.132)
 
-dirname<-"InteractionData"
+dirname<-"Film2_1"
 arena<-ArenaClass(p,dirname)
 
+###################################
+
+###################################
+## Run this part second then, copy 
+## folder (now with results)
+## back to its original location
 data.summary<-Summarize(arena)
 ## If you want to look at data for only a subset of the experiment, you can
 ## pass a range (in minutes)
-Summarize(arena,range=c(0,10))
+##Summarize(arena,range=c(0,10))
 
-Plot(arena, range=c(0,10))
+Plot(arena)
 
 ## You can write the data to a file
 write.csv(
-  summary,
+  data.summary,
   file = paste(dirname, "/DataSummary.csv", sep = ""),
   row.names = FALSE
 )
 
 #or Copy to a clipboard to enter directly into Excel
-write.table(data.summary,"clipboard",sep="\t",row.names=FALSE)
+#write.table(data.summary,"clipboard",sep="\t",row.names=FALSE)
+file = paste(dirname, "/RESULTS", sep = "")
+save.image(file)
+###################################
